b578444a4ecdb184a467cdbec665bb462742a6a4
r13D <ToothPaste32@outlook.com>
2024-03-05 21:26:31 -0500

    add dummy parseWashroomTimes

---
+++ backend/parseWashroomTimes.js
@@ -0,0 +1,50 @@
+async function parseWashroomTimes () {
+    ret = {
+        "Sunday": [
+            {
+                "Start": "9:00",
+                "End": "13:00"
+            },
+            {
+                "Start": "15:00",
+                "End": "20:00"
+            }
+        ],
+        "Monday": [
+            {
+                "Start": "9:00",
+                "End": "13:00"
+            }
+        ],
+        "Tuesday": [
+            {
+                "Start": "9:00",
+                "End": "13:00"
+            }
+        ],
+        "Wednesday": [
+            {
+                "Start": "9:00",
+                "End": "13:00"
+            }
+        ],
+        "Thursday": [
+            {
+                "Start": "9:00",
+                "End": "13:00"
+            }
+        ],
+        "Friday": [
+            {
+                "Start": "9:00",
+                "End": "13:00"
+            }
+        ],
+        "Saturday": [
+            {
+                "Start": "9:00",
+                "End": "13:00"
+            }
+        ],
+    }
+}
