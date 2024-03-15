90c544529e203d08001b9facacbec2e6f3e50119
flight <ToothPaste32@outlook.com>
2024-03-15 16:57:58 -0400

    add news article component

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,22 @@
-
 import React from "react";
 import { View } from "react-native";
 
 import AddWashrooms from "./pages/addWashrooms";
 import UploadWashroomtime from "./pages/uploadWashroomtime";
+import NewsArticle from "./components/newsArticle"
+
 const App = () => {
   return (
   <View style={{ flex: 1 }}>
     {/* Render the addWashrooms component */}
-    <AddWashrooms />
+    {/*<AddWashrooms />*/}
+
 
     {/* Render the uploadWashroomtime component */}
     {/* <UploadWashroomtime /> */}
+
+    {/* Render news article component */}
+    <NewsArticle uid={"testarticle1"}/>
     </View>
   );
 };
