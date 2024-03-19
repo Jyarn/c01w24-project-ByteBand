4ff58efadbf2f86f195eef5e7ea80dda5241a25b
ankhaa14 <ankhjargal.614@gmail.com>
2024-03-19 17:26:51 -0400

    Added add rating feature

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,22 @@
-
 import React from "react";
 import { View } from "react-native";
 
 import AddWashrooms from "./pages/addWashrooms";
 import UploadWashroomtime from "./pages/uploadWashroomtime";
+import AddRatings from "./pages/addRatings";
+
 const App = () => {
   return (
   <View style={{ flex: 1 }}>
     {/* Render the addWashrooms component */}
-    <AddWashrooms />
+    {/* <AddWashrooms /> */}
 
     {/* Render the uploadWashroomtime component */}
     {/* <UploadWashroomtime /> */}
+
+    {/* Render the addRatings component */}
+    {/*** Hard coded to test the addRatings component ***/}
+    <AddRatings washroomId="65f9e3b27d8146a5049b2f77"/>
     </View>
   );
 };
